6e5843529cad7e75a9e366836b9ccbbac3707f59
Dan Spinosa <spinosa@gmail.com>
2012-01-24 14:47:42 -0500

    DRY and refactor terminal for better UX

--- app/assets/javascripts/terminal.js
+++ app/assets/javascripts/terminal.js
@@ -1,99 +1,158 @@
 $(document).ready(function() { $('#main-welcome').hide().delay(250).fadeIn('slow'); }); 
 
+function echoMenu(t, cb){
+	var content = [
+		{ c:'\n', d:1},
+		{ c:'\n', d:1},
+		{ c:"---------------------------------------"},
+		{ c:"Shelby.tv / Project G.O.A.T.S."},
+		{ c:"---------------------------------------"},
+		{ c:'\n', d:1},
+		{ c:"Please Select An Option:"},
+		{ c:'\n', d:1},
+		{ c:"1. Apply                 2. F.A.Q         3. About G.O.A.T.S."},
+		{ c:"4. View Applicants List   5. Access Swag  6. Launch Shelby.tv"},
+		{ c:'\n', d:1},
+		{ c:'\n', d:1}
+	];
+	echoContent(content, t, cb);
+}
+
+function echoContent(content, t, cb){
+	t.echo(content[0].c).addClass(content[0].cssClass || 'default');
+	(content=content.slice(1)).length ? setTimeout( function(){ echoContent(content, t, cb); }, content[0].d || 100) : cb();
+}
+
+function executeTerm(content, t, showMenu, cb){
+	t.clear();
+	t.pause();
+	echoContent(content, t, function(){
+		if(showMenu){
+			echoMenu(t, function(){
+				t.resume();
+				cb && cb();
+			});
+		} else {
+			cb && cb();
+		}
+	});
+	
+}
+
 document.onkeydown = function() {
 	if(event.keyCode == 13) {
 		
-			$('#main-welcome').fadeOut('slow');
+			$('#main-welcome').fadeOut('fast');
 		
-			$('#main').delay(1000).hide().fadeIn('slow').terminal(function(command, term) {
+			$('#main').delay(500).hide().fadeIn('fast').terminal(function(command, term) {
+				var content;
 
-				// Launch Apply Section (1)
-				if(command == '1') {
-					term.clear();
-					term.echo("Coming..");
+				// ------------ (0) INTERNZERO ------------
+				if(command == '0'){
+					var loveLine = Math.random() < 0.3 ? "LOVES YOU" : "LIKES YOU AS A FRIEND";
+					content = [
+						{c:'@INTERNZERO '+loveLine},
+						{c:'\n'}
+					];
+					executeTerm(content, term, true);
 
-					term.echo('\n\n\n\n--------------------------------------- \nShelby.tv / Project G.O.A.T.S.\n---------------------------------------\n\n Please Select An Option:\n\n 1. Apply  2. F.A.Q  3. About G.O.A.T.S.  4. View Applicants List  5. Launch Shelby.tv \n\n\n').css("font-size", "20px").hide().delay(750).fadeIn('slow');
+				// ------------ (1) APPLY ------------
+				} else if(command == '1') {
+					content = [
+						{c:'> APPLY'},
+						{c:'coming soon'}
+					];
+					executeTerm(content, term, true);
 
-				// Launch F.A.Q. Section (2)
+
+				// ------------ (2) FAQ ------------
 				} else if(command == '2') {
-					term.clear();
-					term.echo('> F.A.Q.').hide().delay(600).fadeIn('slow')
+					content = [
+						{c:'> F.A.Q.'},
+						{c:'\n', d:1},
+						{c:'\n', d:1},
+						{c:'WHAT IS PROJECT G.O.A.T.S.?'},
+						{c:'Project G.O.A.T.S. is a top secret interview series powered by Shelby.tv that will take place during SXSW Interactive in Austin, TX on Saturday, March 10th. We are currently accepting applications, but only a limited number of spots are available, and we will not be able to accommodate everyone. You are not required to have a SXSW badge be eligible, but you will be responsible for your transportation to and accommodations in Austin.', cssClass:'faqtext'},
+						{c:'\n', d:1},
+						{c:'\n', d:1},
+						{c:'WHO IS THE CELBRITY GUEST?'},
+						{c:'If you are selected, the identity of the celebrity guest will be revealed to you at the time of your interview.', cssClass:'faqtext'},
+						{c:'\n', d:1},
+						{c:'\n', d:1},
+						{c:'WILL THE INTERVIEW BE FILMED AND PROMOTED FAR AND WIDE?'},
+						{c:'Heck yes it will.', cssClass:'faqtext'},
+						{c:'\n', d:1}
+					];
+					executeTerm(content, term, true);
+					
 
-					term.echo('\n\nWHAT IS PROJECT G.O.A.T.S.?').hide().delay(1500).fadeIn('slow');
-					term.echo('Project G.O.A.T.S. is a top secret interview series powered by Shelby.tv that will take place during SXSW Interactive in Austin, TX on Saturday, March 10th. We are currently accepting applications, but only a limited number of spots are available, and we will not be able to accommodate everyone. You are not required to have a SXSW badge be eligible, but you will be responsible for your transportation to and accommodations in Austin.').hide().delay(2000).fadeIn('slow').addClass('faqtext');
 
-					term.echo('\n\nWHO IS THE CELEBRITY GUEST?').hide().delay(2500).fadeIn('slow');
-					term.echo('If you are selected, the identity of the celebrity guest will be revealed to you at the time of your interview.').hide().delay(3000).fadeIn('slow').addClass('faqtext');
-
-					term.echo('\n\nWILL THE INTERVIEW BE FILMED AND PROMOTED FAR AND WIDE?').hide().delay(3500).fadeIn('slow');
-					term.echo('Heck yes it will. ').hide().delay(4000).fadeIn('slow').addClass('faqtext');
-
-					term.echo('\n\n--------------------------------------- \nShelby.tv / Project G.O.A.T.S.\n---------------------------------------\n\n Please Select An Option:\n\n 1. Apply  2. F.A.Q  3. About G.O.A.T.S.  4. View Applicants List  5. Launch Shelby.tv \n\n\n').css("font-size", "20px").hide().delay(5000).fadeIn('slow');
-
-					// Launch About G.O.A.T.S. Section (3)
+				// ------------ (3) ABOUT G.O.A.T.S. ------------
 				} else if(command == '3') {
+					content = [
+						{c:'> ABOUT G.O.A.T.S.'},
+						{c:'\n', d:1},
+						{c:'\n', d:1},
+						{c:'LOADING GOATS INFORMATION...'},
+						{c:'>', d:200},
+						{c:'>', d:200},
+						{c:'>', d:200}
+					];
+					executeTerm(content, term, false, function(){
+						var ranLinks = [
+							"http://lolgoats.com/",
+							"http://shel.tv/t1omt9",
+							"http://img.artknowledgenews.com/files2010jan/Robert-Rauschenberg-Monogram.jpg" ];
+						window.location = ranLinks[Math.floor(Math.random() * ranLinks.length)];
+					});
 
-					term.clear();
-					term.echo('> ABOUT G.O.A.T.S.').hide().delay(600).fadeIn('slow')
-					term.echo('\n\nLOADING GOATS INFORMATION...').hide().delay(1500).fadeIn('slow').css("font-size", "20px");
-					term.echo('\n>').hide().delay(1700).fadeIn('slow').css("font-size", "20px");
-					term.echo('\n>').hide().delay(1900).fadeIn('slow').css("font-size", "20px");
-					term.echo('\n>').hide().delay(2100).fadeIn('slow').css("font-size", "20px");
-
-					// Load Random Link Within Array, Section (3)
-					var ranLinks = new Array();
-					ranLinks[0] = "http://lolgoats.com/"
-					ranLinks[1] = "http://shel.tv/t1omt9"
-					ranLinks[2] = "http://img.artknowledgenews.com/files2010jan/Robert-Rauschenberg-Monogram.jpg"
-
-					function ranLink() {
-						window.open('' + ranLinks[Math.floor(Math.random() * ranLinks.length)] + '')
-					}
 
 
-					window.setTimeout(ranLink, 3000);
-
-					term.echo('\n\n\n\n--------------------------------------- \nShelby.tv / Project G.O.A.T.S.\n---------------------------------------\n\n Please Select An Option:\n\n 1. Apply  2. F.A.Q  3. About G.O.A.T.S.  4. View Applicants List  5. Launch Shelby.tv \n\n\n').css("font-size", "20px").hide().delay(2300).fadeIn('slow');
-
-					// Launch Applicant List Section (4)
+				// ------------ (4) APPLICANT LIST ------------
 				} else if(command == '4') {
 					term.clear();
-					term.echo('> APPLICANT LIST').hide().delay(600).fadeIn('slow')
-					term.echo('\n\Running "Applicant List"...').hide().delay(1500).fadeIn('slow').css("font-size", "20px");
+					term.echo('> APPLICANT LIST').hide().delay(600).fadeIn('slow');
+					term.echo('\nRunning "Applicant List"...').hide().delay(1500).fadeIn('slow').css("font-size", "20px");
 					term.echo('\n>').hide().delay(1700).fadeIn('slow').css("font-size", "20px");
 					term.echo('\n>').hide().delay(1900).fadeIn('slow').css("font-size", "20px");
 					term.echo('\n>').hide().delay(2100).fadeIn('slow').css("font-size", "20px");
 					term.echo('\nCarol   Mike    Greg   Marcia   Peter   Jan\nBobby   Cindy   Alice   Sam').hide().delay(2300).fadeIn('slow').css("font-size", "20px");
 
 					term.echo('\n\n\n\n--------------------------------------- \nShelby.tv / Project G.O.A.T.S.\n---------------------------------------\n\n Please Select An Option:\n\n 1. Apply  2. F.A.Q  3. About G.O.A.T.S.  4. View Applicants List  5. Launch Shelby.tv \n\n\n').css("font-size", "20px").hide().delay(2600).fadeIn('slow');
-					term.e
+					
+					
+					
+				// ------------ (5) SHELBY.TV ------------
 				} else if(command == '5') {
-					term.clear();
-					term.echo('> LAUNCH SHELBY.TV').hide().delay(600).fadeIn('slow')
-					term.echo('\n\nLOADING SHELBY.TV...').hide().delay(1500).fadeIn('slow').css("font-size", "20px");
-					term.echo('\n>').hide().delay(1700).fadeIn('slow').css("font-size", "20px");
-					term.echo('\n>').hide().delay(1900).fadeIn('slow').css("font-size", "20px");
-					term.echo('\n>').hide().delay(2100).fadeIn('slow').css("font-size", "20px");
+					content = [
+						{c:'> LAUNCH SHELBY.TV'},
+						{c:'\n', d:1},
+						{c:'\n', d:1},
+						{c:'LOADING SHELBY.TV...'},
+						{c:'>', d:200},
+						{c:'>', d:200},
+						{c:'>', d:200}
+					];
+					executeTerm(content, term, false, function(){
+						window.location = "http://shelby.tv";
+					});					
 
-					function shelbyLink() {
-						window.open('http://www.shelby.tv')
-					}
 
 
-					window.setTimeout(shelbyLink, 3000);
 
-					term.echo('\n\n\n\n--------------------------------------- \nShelby.tv / Project G.O.A.T.S.\n---------------------------------------\n\n Please Select An Option:\n\n 1. Apply  2. F.A.Q  3. About G.O.A.T.S.  4. View Applicants List  5. Launch Shelby.tv \n\n\n').css("font-size", "20px").hide().delay(2300).fadeIn('slow');
-
-					// If any commands are entered outside of the above parameters
+				// ------------ bad command ------------
 				} else {
-					term.echo('Unknown Command: ' + command + ' ').css("color", "#870000")
+					term.error('Unknown Command: ' + command + ' ');
 				}
+				
+				//keep scrolling
+				$("body").scrollTop($(".terminal-output").height());
 			}, {
-
-				greetings : "Main Screen On..\n" + "> \n" + "We have signal! \n" + "> \n\n\n" + "--------------------------------------- \nShelby.tv / Project G.O.A.T.S.\n---------------------------------------\n\n" + "Please Select An Option:\n\n" + "1.  Apply \n" + "2.  F.A.Q \n" + "3.  About G.O.A.T.S. \n" + "4.  View Applicants List \n" + "5.  Launch Shelby.tv \n\n\n\n",
+				greetings : "Main Screen On...\n" + "> \n" + "We have signal! \n" + "> \n\n\n",
 				prompt : 'Selection:',
-				height : 600,
-				width : 1200
+				height : '100%',
+				width : '100%',
+				onInit: function(t){ echoMenu(t); }
 			});
 	}
-}
+};
