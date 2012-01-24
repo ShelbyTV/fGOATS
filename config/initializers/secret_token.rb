b91e9da6949e4b1e4e0797e5f286b4fa57d56321
Dan Spinosa <spinosa@gmail.com>
2012-01-24 11:26:55 -0500

    blank rails 3 mongo app for fgoats project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fgoats::Application.config.secret_token = 'e343a32ed4291a172b171e0b3d662afb3ed0f16f2a894c9edc73b2253fb8cb236ff20de51a92d17894e2bf8f121993e8ccf5242571807f6be14a3c9e7f08cae3'
