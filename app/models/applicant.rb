7005fdd4e8dc0b81d7f748f47bf1719c7276287c
Dan Spinosa <spinosa@gmail.com>
2012-02-21 11:35:16 -0500

    method on applicant to easily put comma delimited list

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -7,4 +7,10 @@ class Applicant
   key :job_title,     String
   key :email,         String
 
+  def self.puts_comma_list
+    puts "first_name, last_name, company, job_title, email"
+    Applicant.all.each { |a| puts "#{a.first_name}, #{a.last_name}, #{a.company}, #{a.job_title}, #{a.email}" }
+  end
+    
+
 end
